146786ae0776152245077098982df8e56f2bb3a6
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-07-05 14:52:34 +0530

    Initial commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,67 +1,61 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { BsFillGrid3X3GapFill } from 'react-icons/bs'
+import React from 'react';
+import { BsFillGrid3X3GapFill } from 'react-icons/bs';
 import { FaHome } from 'react-icons/fa';
-
 
 function Sidebar() {
   return (
     <aside id="sidebar">
       <div className='sidebar-title'>
         <div className='sidebar-brand' style={{fontSize:'30px', fontWeight:'bold'}}>
-       hava havai
+          hava havai
         </div>
         <span className='icon close_icon'>X</span>
       </div>
       <ul className='sidebar-list'>
         <li className='sidebar-list-item'>
-            <a href=''>
-                <FaHome className='icon'/> Home
-            </a>
+          <a href='/' onClick={(e) => e.preventDefault()}>
+            <FaHome className='icon'/> Home
+          </a>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                <BsFillGrid3X3GapFill className='icon'/> Dashboard
-            </a>
+          <a href='/dashboard' onClick={(e) => e.preventDefault()}>
+            <BsFillGrid3X3GapFill className='icon'/> Dashboard
+          </a>
         </li>
         <li className='sidebar-list-item' style={{fontWeight:'bold', fontSize:'20px'}}>
-            <a href=''>
-                Services
-            </a>
+          <span>Services</span>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                Airports
-            </a>
+          <a href='/airports' onClick={(e) => e.preventDefault()}>
+            Airports
+          </a>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                Videos
-            </a>
+          <a href='/videos' onClick={(e) => e.preventDefault()}>
+            Videos
+          </a>
         </li>
         <li className='sidebar-list-item' style={{fontWeight:'bold', fontSize:'20px'}}>
-            <a href=''>
-            Others
-            </a>
+          <span>Others</span>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                List 1
-            </a>
+          <a href='/list1' onClick={(e) => e.preventDefault()}>
+            List 1
+          </a>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                List 2
-            </a>
+          <a href='/list2' onClick={(e) => e.preventDefault()}>
+            List 2
+          </a>
         </li>
         <li className='sidebar-list-item' style={{paddingTop:'0px'}}>
-            <a href=''>
-                List 3
-            </a>
+          <a href='/list3' onClick={(e) => e.preventDefault()}>
+            List 3
+          </a>
         </li>
       </ul>
     </aside>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
