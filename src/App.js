8fc64a93d1eff1ee3d951715e90c63091de6cb06
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-07-05 14:08:33 +0530

    Message describing your changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Airports from './components/Airports';
+import AirportPage from './components/AirportPage';
+import Sidebar from './components/Sidebar';
+import AddTerminalPopup from './components/AddTerminalPopup'; 
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Sidebar/>
+        <Routes>
+          <Route index element={<Airports />} />
+          <Route path="airport/:id" element={<AirportPage />} />
+        </Routes>
+        <AddTerminalPopup /> {/* Render AddTerminalPopup outside of Routes */}
+      </div>
+    </Router>
   );
 }
 
